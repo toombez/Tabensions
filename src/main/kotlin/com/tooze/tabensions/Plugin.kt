affe969c3379707a013d4c256a4af2200914c680
Timur <timurtokaev.toombez@gmail.com>
2022-11-09 02:28:20 +0300

    Warning if PlaceholderAPI not installed

--- src/main/kotlin/com/tooze/tabensions/Plugin.kt
+++ src/main/kotlin/com/tooze/tabensions/Plugin.kt
@@ -1,9 +1,15 @@
 package com.tooze.tabensions
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin : JavaPlugin() {
     override fun onEnable() {
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") === null) {
+            logger.warning("Cannot find PlaceholderAPI plugin. Disabling plugin.")
+
+            Bukkit.getPluginManager().disablePlugin(this)
+        }
     }
 
     override fun onDisable() {

e1e085518cb6f871bf7da77fa864c36f600f280e
Timur <timurtokaev.toombez@gmail.com>
2022-11-09 02:13:25 +0300

    Add TabensionsExpansion class

---
+++ src/main/kotlin/com/tooze/tabensions/TabensionsExpansion.kt
@@ -0,0 +1,17 @@
+package com.tooze.tabensions
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion
+
+class TabensionsExpansion : PlaceholderExpansion() {
+    override fun getAuthor(): String {
+        return "toombez"
+    }
+
+    override fun getIdentifier(): String {
+        return "tabensions"
+    }
+
+    override fun getVersion(): String {
+        return "0.0.1"
+    }
+}

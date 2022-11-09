5810531ded34c58b936ab301fadb17a34fb09346
Timur <timurtokaev.toombez@gmail.com>
2022-11-09 06:35:09 +0300

    Dimension placeholder

--- src/main/kotlin/com/tooze/tabensions/TabensionsExpansion.kt
+++ src/main/kotlin/com/tooze/tabensions/TabensionsExpansion.kt
@@ -1,6 +1,8 @@
 package com.tooze.tabensions
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion
+import org.bukkit.ChatColor
+import org.bukkit.OfflinePlayer
 
 class TabensionsExpansion(private val plugin: Plugin) : PlaceholderExpansion() {
     override fun getAuthor(): String {
@@ -13,5 +15,28 @@ class TabensionsExpansion(private val plugin: Plugin) : PlaceholderExpansion() {
 
     override fun getVersion(): String {
         return plugin.description.version
+    }
+
+    override fun onRequest(p: OfflinePlayer?, params: String): String? {
+        // Dimension placeholder
+        if (params.equals("dimension", true)) {
+            if (p === null || p.player === null || !p.player!!.isOnline) {
+                return null
+            }
+
+            // Check ignored worlds
+            val world = p.player!!.world
+            if (plugin.ignoredWorlds.contains(world.name)) {
+                return null
+            }
+
+            // Placeholder
+            val environment = world.environment
+            val char = plugin.char
+            val charCode = plugin.colors[environment]!!
+            return ChatColor.translateAlternateColorCodes('§', "${charCode}${char}")
+        }
+
+        return null
     }
 }

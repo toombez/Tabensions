9493ddbe24976e089dcbaf8d96a087d8d9e73862
Timur <81884993+toombez@users.noreply.github.com>
2022-11-09 06:48:03 +0300

    Feat/dimension placeholder (#3)
    * Init once config values
    
    * Dimension placeholder

--- src/main/kotlin/com/tooze/tabensions/Plugin.kt
+++ src/main/kotlin/com/tooze/tabensions/Plugin.kt
@@ -1,19 +1,28 @@
 package com.tooze.tabensions
 
 import org.bukkit.Bukkit
+import org.bukkit.World.Environment
 import org.bukkit.plugin.java.JavaPlugin
 
 class Plugin : JavaPlugin() {
+    val char: String = config.getString("dimensionChar").toString()
+    val colors: Map<Environment, String> = Environment
+        .values()
+        .associateWith { config.getString("colors.${it.toString()}")!! }
+    val ignoredWorlds: List<String> = config.getStringList("ignoredWorlds")
+
     override fun onEnable() {
+        config.options().copyDefaults()
+        saveDefaultConfig()
+
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") === null) {
             logger.warning("Cannot find PlaceholderAPI plugin. Disabling plugin.")
 
             Bukkit.getPluginManager().disablePlugin(this)
-        } else {
-            TabensionsExpansion(this).register()
+            return
         }
 
-        saveDefaultConfig()
+        TabensionsExpansion(this).register()
     }
 
     override fun onDisable() {
